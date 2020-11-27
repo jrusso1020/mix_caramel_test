239609abaffde0ad9e45f89abe2d3b1b2817dbf9
jrusso1020 <jdrusso1020@gmail.com>
2020-11-26 22:29:36 -0500

    add some docs and format

--- lib/mix/tasks/compiler/caramel.ex
+++ lib/mix/tasks/compiler/caramel.ex
@@ -1,16 +1,27 @@
 defmodule Mix.Tasks.Compile.Caramel do
+  @moduledoc """
+  This module is responsible for compiling caramel code
+
+  It looks for code in the src/ directory and then will compile all the code
+  and mode it to the gen/src/ directory so it is available in your mix project.
+  """
   use Mix.Task.Compiler
 
   def run(_args) do
     case Mix.shell().cmd("caramelc compile src/*.ml") do
       0 ->
         File.mkdir_p!(Path.dirname("gen/src/"))
+
         "*.erl"
         |> Path.wildcard()
-        |> Enum.each(fn generated_file -> File.rename!(generated_file, "gen/src/#{Path.basename(generated_file)}") end)
+        |> Enum.each(fn generated_file ->
+          File.rename!(generated_file, "gen/src/#{Path.basename(generated_file)}")
+        end)
 
         {:ok, []}
-      status -> exit(status)
+
+      status ->
+        exit(status)
     end
   end
 end
